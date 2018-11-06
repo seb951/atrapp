6115f254950ecf70bf709e1908310f30d3b4b0b0
Sebastien Renaut <sebastien.renaut@gmail.com>
2018-11-06 16:36:33 -0500

    minor update to env. data graphs

--- Rcode/env_data.R
+++ Rcode/env_data.R
@@ -1,11 +1,10 @@
 
 
-setwd("/Users/jerry/Dropbox/CSBQ/shapiro")
+setwd("/Users/jerry/Documents/CSBQ/shapiro")
 
 library(lubridate)
 
 #load env. data
-
 env = read.table("results/GRDI_ECO_formated_Metadata_CNRC_2016_for_stats_Jesse_v1seb.txt",
                  header = F, stringsAsFactors = F,sep = "\t",comment.char = "",skip = 1)
 
@@ -14,16 +13,18 @@ header  = read.table("results/GRDI_ECO_formated_Metadata_CNRC_2016_for_stats_Jes
 
 env = env[,1:91]
 colnames(env) = header[1,1:91]
-
 env[,4] = mdy(paste(env[,4]," 2016"))
 
-
-dev.new(width=7, height=7,noRStudioGD = TRUE)
+for(graph in c("pdf", "png"))
+{
+if(graph == "pdf") { dev.new(width=7, height=7,noRStudioGD = TRUE) }
+if(graph == "png") { png("figures/environmental_data.png",width=7, res =400,height=7,units= 'in') }
 par(mfrow = c(3,1))
 #N (mg N/L)
 plot(env[env[,3]=="station1",c(4,6)],type = "p",col = "darkblue",pch =20,cex =2,
      ylim = c(0,max(env[env[,3]=="station1",6])*1.1))
 #points(env[env[,3]=="station1",c(4,6)],type = "l",col = "black",pch =20)
+x = env[env[,3]=="station1",c(4)]
 axis(3,at=x,labels=x,cex.axis=0.8,las=2,tick=F,line=-7)
 #text(env[40,4]-15,env[40,6],env[40,4],xpd = T,col="darkblue")
 
@@ -41,7 +42,13 @@ axis(3,at=x,labels=x,cex.axis=0.8,las=2,tick=F,line=-7)
 #points(env[env[,3]=="station1",c(4,27)],type = "l",col = "black",pch =20)
 #text(env[40,4]-15,as.numeric(env[40,27]),env[40,4],xpd = T,col="darkblue")
 
-dev.print(device=pdf, "figures/env_data.pdf", onefile=FALSE)
+
+#PDF
+if(graph == "pdf")  {  dev.print(device=pdf, "figures/environmental_data.pdf", onefile=FALSE)  }
+
 dev.off()
+}
+
+
 
 
