d457f8c26d1a2a556c3c1ebb90700367aa9657b9
Sebastien Renaut <sebastien.renaut@gmail.com>
2018-11-07 10:36:40 -0500

    ggplot environmental data

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# atrapp
-repo with the main results of the Lake Champlain metatranscriptomics analyses
-sebastien.renaut@gmail.com
-fall 2018
+# atrapp  
+repo with the main results of the Lake Champlain metatranscriptomics analyses  
+sebastien.renaut@gmail.com  
+fall 2018  
 
--- Rcode/env_data.R
+++ Rcode/env_data.R
@@ -3,7 +3,7 @@
 setwd("/Users/jerry/Documents/CSBQ/shapiro")
 
 library(lubridate)
-
+library(ggplot)
 #load env. data
 env = read.table("results/GRDI_ECO_formated_Metadata_CNRC_2016_for_stats_Jesse_v1seb.txt",
                  header = F, stringsAsFactors = F,sep = "\t",comment.char = "",skip = 1)
@@ -15,39 +15,32 @@ env = env[,1:91]
 colnames(env) = header[1,1:91]
 env[,4] = mdy(paste(env[,4]," 2016"))
 
-for(graph in c("pdf", "png"))
-{
-if(graph == "pdf") { dev.new(width=7, height=7,noRStudioGD = TRUE) }
-if(graph == "png") { png("figures/environmental_data.png",width=7, res =400,height=7,units= 'in') }
-par(mfrow = c(3,1))
-#N (mg N/L)
-plot(env[env[,3]=="station1",c(4,6)],type = "p",col = "darkblue",pch =20,cex =2,
-     ylim = c(0,max(env[env[,3]=="station1",6])*1.1))
-#points(env[env[,3]=="station1",c(4,6)],type = "l",col = "black",pch =20)
-x = env[env[,3]=="station1",c(4)]
-axis(3,at=x,labels=x,cex.axis=0.8,las=2,tick=F,line=-7)
-#text(env[40,4]-15,env[40,6],env[40,4],xpd = T,col="darkblue")
-
-#P (µg P/L)
-plot(env[env[,3]=="station1",c(4,21)],type = "p",col = "darkblue",pch =20,cex =2,
-     ylim = c(0,max(env[env[,3]=="station1",21])*1.1))
-axis(3,at=x,labels=x,cex.axis=0.8,las=2,tick=F,line=-7)
-#points(env[env[,3]=="station1",c(4,21)],type = "l",col = "black",pch =20)
-#text(env[40,4]-15,env[40,21],env[40,4],xpd = T,col="darkblue")
-
-#Chlorophyll (filtration; ug/L)
-plot(env[env[,3]=="station1",c(4,27)],type = "p",col = "darkblue",pch =20,cex =2,
-     ylim = c(0,600))
-axis(3,at=x,labels=x,cex.axis=0.8,las=2,tick=F,line=-7)
-#points(env[env[,3]=="station1",c(4,27)],type = "l",col = "black",pch =20)
-#text(env[40,4]-15,as.numeric(env[40,27]),env[40,4],xpd = T,col="darkblue")
+env_ggplot = data.frame(values = as.numeric(unlist(env[,c(6,21,27)])),date = env[,4],var1 = c(rep("N",44),rep("P",44),rep("Chl",44)))
+env_ggplot$var2 <- factor(env_ggplot$var1, labels = c("Chlorophyl A\n(ug/L)", "Nitrogen\n(mg N/L)l", "Phosphorus\n(ug P/L)"))
 
 
+
+#graph = "pdf"
+graph = "png"
+
+if(graph == "pdf") { dev.new(width=6, height=4,noRStudioGD = TRUE) }
+if(graph == "png") { png("figures/environmental_data.png",width=6, res =400,height=4,units= 'in') }
+  
+  p = ggplot() + labs(title = "Lake Champlain") +
+  geom_point(aes(x = date, y = values),na.rm=T,data = env_ggplot) + 
+    theme_bw() + 
+    ylab("Concentration") +
+    xlab("Sampling date") +
+    theme(plot.title = element_text(hjust = 0.5, size=14, face="bold")) +
+    scale_x_date(date_breaks = "months" , date_labels = "%b") +
+    facet_grid(rows=vars(var2),scales="free")
+ 
+  p
+  
 #PDF
 if(graph == "pdf")  {  dev.print(device=pdf, "figures/environmental_data.pdf", onefile=FALSE)  }
 
 dev.off()
-}
 
 
 
